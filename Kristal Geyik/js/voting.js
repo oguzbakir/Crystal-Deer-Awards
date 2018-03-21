8f1e10f0e2938eb7a3e2e288dcbc7d5140743682
ZE0TRON <cmpbilge@gmail.com>
2018-03-21 13:22:39 +0300

    site is ready to release

--- Kristal Geyik/js/voting.js
+++ Kristal Geyik/js/voting.js
@@ -10,23 +10,25 @@ ddoptions ={
     YönetimFinans: ['Yönetim','Finans','Girişimclik'],
     Sanat: ['Müzik','Resim','Sinema'],
     Mühendislik: ['Yazılım','Elektronik','İnşaat'],
-    Sanat15Müzik:['Nicola Cruz','Nicolas Jaar','Glitch Mob'],
-    Sanat15Sinema:['The Dark Knight','Lord of the Rings','Harry Potter'],
-    Sanat15Müzik15Nicola26Cruz:['images/candidates/nicolacruz.jpg','Nicola Cruz <br>  Ecuador\'s musical treasure, catch Nicola Cruz\'s hourlong live set of his remarkable downtempo Andean electronic music.'],
-    Sanat15Müzik15Glitch26Mob:['images/candidates/glitchmob.jpg','The Glitch Mob is an American electronic music group from Los Angeles, California. It consists of edIT, Boreta and Ooah. Chris Martins of LA Weekly noted that they "have undoubtedly found the largest audience of any L.A. beat scene artist yet.']
+    Müzik:['Nicola Cruz','Nicolas Jaar','Glitch Mob'],
+    Sinema:['The Dark Knight','Lord of the Rings','Harry Potter'],
+    Nicola26Cruz:['images/candidates/nicolacruz.jpg','Nicola Cruz <br>  Ecuador\'s musical treasure, catch Nicola Cruz\'s hourlong live set of his remarkable downtempo Andean electronic music.'],
+    Glitch26Mob:['images/candidates/glitchmob.jpg','The Glitch Mob is an American electronic music group from Los Angeles, California. It consists of edIT, Boreta and Ooah. Chris Martins of LA Weekly noted that they "have undoubtedly found the largest audience of any L.A. beat scene artist yet.']
 };
 $(document).ready(function(){
+    var d = new Date();
+    $('#kalanZaman').html(31-d.getDate()+2+" Gün");
     $('#search-select')
         .dropdown({
             onChange: function (value,text,$selectedItem) {
-                console.log(value);
+                //console.log(value);
                 var o = $('#sub-search-select');
                 o.empty();
                 var c= ddoptions[value];
                 createOption('Alt Kategori Seciniz',o,"");
                 for(var p in c){
-                    console.log("'creating option : "+value+"15"+c[p]);
-                    createOption(c[p],o,value+"15"+c[p]);
+                    //console.log("creating option : "+c[p]);
+                    createOption(c[p],o,c[p]);
                 }
             }
         })
@@ -34,15 +36,15 @@ $(document).ready(function(){
     $('#sub-search-select')
         .dropdown({
             onChange: function (value,text,$selectedItem) {
-                console.log(value);
+                //console.log(value);
                 var o = $('#candidate-search-select');
                 o.empty();
-                console.log(value);
+                //console.log(value);
                 var c= ddoptions[value];
                 createOption('Aday',o,"");
                 for(var p in c){
-
-                    createOption(c[p],o,value+"15"+c[p].replace(' ','26'));
+                    //console.log("creating option : "+c[p].replace(' ',26));
+                    createOption(c[p],o,c[p].replace(' ','26'));
                 }
             }
         })
@@ -50,31 +52,14 @@ $(document).ready(function(){
     $('#candidate-search-select')
         .dropdown({
                 onChange: function (value,text,$selectedItem) {
-                    console.log(value);
+                    //console.log(value);
                     $('#AdayPhoto').attr('src',ddoptions[value][0]);
                     $('#AdayDetay').html(ddoptions[value][1]);
+                    $('#oyverButton').attr('href',"https://docs.google.com/forms/d/e/1FAIpQLSdlzDpbdKOb1OcBaq_dcHReaQkmK5lGXEjEhXjFOl_LJ08q3Q/viewform?usp=pp_url&entry.1755287923="+$('#candidate-search-select').val().replace('26','+')+"&entry.1888246541");
                 }
             }
 
         )
     ;
-    $('#oyverButton').click(function (){
-        jQuery.ajax({
-            type: "POST",
-            url: 'voting.php',
-            dataType: 'json',
-            data: {functionname: 'send_vote', arguments: $('#candidate-search-select').val()},
-
-            success: function (obj, textstatus) {
-                if( !('error' in obj) ) {
-                    yourVariable = obj.result;
-                }
-                else {
-                    console.log(obj.error);
-                }
-            }
-        });
-
-    });
 
 });
